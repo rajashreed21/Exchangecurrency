a56dff74321642197a4df8683497b633c8d42928
RPS <rps@RPSs-Mac-80.local>
2024-06-12 21:59:44 +0530

    currency Swift

---
+++ CurrencyExchange/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  CurrencyExchange
+//
+//  Created by RPS on 12/06/24.
+//
+
+import SwiftUI
+
+struct ExchangeRate: Codable {
+    let currency: String
+    let rate: Double
+}
+
+// View model responsible for fetching exchange rates
+class CurrencyViewModel: ObservableObject {
+    @Published var exchangeRates: [ExchangeRate] = []
+    
+    init() {
+        fetchExchangeRates()
+    }
+    func fetchExchangeRates() {
+        guard let url = URL(string: "https://exchangecurrency-c819.onrender.com/exchange-rates") else {
+            print("Invalid API URL")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error fetching data:", error.localizedDescription)
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                print("Invalid HTTP response")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data received from the server")
+                return
+            }
+            
+            do {
+                let decodedData = try JSONDecoder().decode([ExchangeRate].self, from: data)
+                self.exchangeRates = decodedData
+            } catch {
+                print("Error decoding JSON:", error)
+                print("Received JSON data:", String(data: data, encoding: .utf8) ?? "Invalid JSON")
+            }
+        }.resume()
+    }
+
+}
+
+// Main content view displaying exchange rates in a list
+struct ContentView: View {
+    @ObservedObject var viewModel = CurrencyViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List(viewModel.exchangeRates, id: \.currency) { rate in
+                VStack(alignment: .leading) {
+                    Text(rate.currency)
+                        .font(.headline)
+                    Text("Rate: \(rate.rate, specifier: "%.2f")")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .navigationBarTitle("Exchange Rates")
+        }
+    }
+}
+
+// Preview provider for ContentView
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
